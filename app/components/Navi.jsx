9fd430f0674fdca3abdfd3993e28cc4e63843e48
Juha Paananen <juha.paananen@gmail.com>
2023-02-17 16:20:28 +0200

    Add a Next.js app

---
+++ app/components/Navi.jsx
@@ -0,0 +1,17 @@
+export default function Navi() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/git">Git</a>
+        </li>
+        <li>
+          <a>Web</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ app/pages/git.jsx
@@ -0,0 +1,15 @@
+import Navi from "../components/Navi";
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <h1>Git stuff</h1>
+        <Navi />
+      </header>
+      <main>
+        <p>Git is like riding in a limo.</p>
+      </main>
+    </>
+  );
+}
