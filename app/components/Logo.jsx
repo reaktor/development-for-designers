5ed429c22f15751040544793a3160e0333a3e1e1
annaheini <annaheini@reaktor.fi>
2023-03-17 16:43:14 +0200

    Moved logo styles to a module cand removed logo styles from styles folder.

--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -1,6 +1,8 @@
+import styles from './Logo.module.css';
+
 export default function Logo() {
     return (
-        <p id="Logo"><a href="/"> Dev 4 Designers</a></p>
+        <p className={styles.Logo}><a href="/"> Dev 4 Designers</a></p>
     );
 
 }
